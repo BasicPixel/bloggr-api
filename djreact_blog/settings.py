cc3bc33e90c530e73ff53f3cd2e466bb50352805
O. Qudah <69857856+BasicPixel@users.noreply.github.com>
2022-01-12 21:27:39 +0200

    Add frontend url to CORS whitelist

--- djreact_blog/settings.py
+++ djreact_blog/settings.py
@@ -134,7 +134,7 @@ STATIC_URL = '/static/'
 #     os.path.join(BASE_DIR, 'staticfiles'),
 # )
 
-CORS_ORIGIN_WHITELIST = ('http://localhost:3000',)
+CORS_ORIGIN_WHITELIST = ('http://localhost:3000', 'https://bloggr.vercel.app/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
